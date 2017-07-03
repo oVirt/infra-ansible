744321b6b85e83bc75ca1cf5a279ba7cc1b84bc5
Marc Dequènes (Duck) <duck@redhat.com>
2017-07-04 03:04:58 +0900

    Update CI tests
    Also fixed mistakes reported by ansible-lint to ensure these changes do
    not block the review (tasks without names).
    Also fixed requirements URLs so Ansible Galaxy works well again.
    Also added the missing `tor` role requirement.
    
    Change-Id: Ic65e682ad7594d8bfff3f979708f7ca8547156e0

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -1,11 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 # check-patch.sh - Automated tests for patches
 #
 
-# TODO: Add real tests here, this just checks the tools we need are installed
+set -o nounset -o errexit -o pipefail -o xtrace
+
+# check the tools's version, useful for debugging
 echo ============================ Ansible version ===========================
 ansible --version
 echo ========================= Ansible-lint version =========================
 ansible-lint --version
 echo ============================ flake8 version ============================
 flake8 --version
+
+
+echo ============================= flake8 check =============================
+flake8 --exclude=$(find . -mindepth 2 -name ".git" -printf "%h,") --ignore=E126,E131,E501,E303 .
+
+echo ========================== Ansible-lint check ==========================
+ansible-lint -p --nocolor $(find . -mindepth 2 -name ".git" -printf "--exclude=%h ") playbooks/*.yml 
+
+echo ======================== Ansible Galaxy install=========================
+ansible-galaxy install -r requirements.yml
+
+echo ========================= Ansible syntax check =========================
+# until ANSIBLE_VAULT_PASSWORD_FILE support is added to Jenkins, delete Vault files (which are then not checked)
+find . -name '*.vault.*' -delete
+find playbooks/ -maxdepth 1 -name '*.yml' | xargs -n 1 ansible-playbook --syntax-check
+
+# TODO: some actions cannot be checked properly with `ansible-playbook --check`
+#       we should use `check_mode` when necessary but it is missing in many roles
+#       also to do it properly we should test each each host installation in a discardable dedicated sub-container/VM
+
